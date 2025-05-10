3adb53c282cce51603c7ade91fa2e0cc949fa818
Andreea <andreeaflorinabarla@yahoo.com>
2025-05-10 13:57:14 +0300

    message

--- next-app/next.config.mjs
+++ next-app/next.config.mjs
@@ -1,14 +1,9 @@
 
-import path from 'path';
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  output: 'standalone',
-  webpack: (config) => {
-    config.resolve.alias['@'] = path.resolve(__dirname);
-    return config;
-  },
+  output: "standalone",
 };
 
 export default nextConfig;
